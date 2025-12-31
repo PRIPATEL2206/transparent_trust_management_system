aa461939b6ca55067745d24202a0f04e92a0466b
skillboost2024 <skillboost2024@gmail.com>
2025-12-31 06:58:19 +0000

    setup done for djnago and tailwind

---
+++ .idx/dev.nix
@@ -0,0 +1,22 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.python311 # Python 3.11 interpreter
+    pkgs.python311Packages.pip
+    pkgs.openssh
+  ];
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "google.gemini-cli-vscode-ide-companion"
+    ];
+  };
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is djnago app build using django and tailwindcss
